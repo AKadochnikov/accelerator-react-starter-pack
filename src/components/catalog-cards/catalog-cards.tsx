1bfde8afd05966c12d092262bdb3b65acb9d88e7
Alexander <alex.kadoh@mail.ru>
2021-12-30 18:34:54 +1000

    create catalogCard and Card components

---
+++ src/components/catalog-cards/catalog-cards.tsx
@@ -0,0 +1,11 @@
+import Card from '../card/card';
+
+function CatalogCards (): JSX.Element {
+  return (
+    <div className="cards catalog__cards">
+      <Card/>
+    </div>
+  );
+}
+
+export default CatalogCards;
