e0d58a8943cba80f7a10da8d575fbfb29503e1d0
Alexander <alex.kadoh@mail.ru>
2022-01-17 21:46:35 +1000

    create test for FilterStringsCount component

---
+++ src/components/filter-strings-count/filter-strings-count.test.tsx
@@ -0,0 +1,23 @@
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store, history} from '../mockStore';
+import FilterStringsCount from './filter-strings-count';
+
+describe('Component: FilterPrice', () => {
+  const fakeOnChange = jest.fn();
+  const fakeGuitarTypes = [''];
+  const fakeGuitarCounts = [3];
+
+  it('should render FilterPrice component', () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <FilterStringsCount onChange={fakeOnChange} newGuitarTypes={fakeGuitarTypes} newGuitarCounts={fakeGuitarCounts}/>
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getByText(/Количество струн/i)).toBeInTheDocument();
+  });
+} );
