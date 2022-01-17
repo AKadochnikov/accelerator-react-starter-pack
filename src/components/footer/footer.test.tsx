64bbe5ec4f10e0bf187d4d4864e966a0bb3a409e
Alexander <alex.kadoh@mail.ru>
2022-01-17 21:53:45 +1000

    create test for Footer component

---
+++ src/components/footer/footer.test.tsx
@@ -0,0 +1,14 @@
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {history} from '../mockStore';
+import Footer from './footer';
+
+describe('Component: Footer', () => {
+  it('should render Footer component', () => {
+    render(<Router history={history}><Footer/></Router>);
+
+    expect(screen.getByText(/Режим работы/)).toBeInTheDocument();
+    expect(screen.getByText(/без выходных/)).toBeInTheDocument();
+    expect(screen.getByText(/Сервис-центры/)).toBeInTheDocument();
+  });
+});
