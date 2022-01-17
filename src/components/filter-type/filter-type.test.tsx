a631346b95d0f47069761b20166c7b21f4946ff4
Alexander <alex.kadoh@mail.ru>
2022-01-17 21:50:44 +1000

    create test for FilterType component

--- src/components/filter-type/filter-type.test.tsx
+++ src/components/filter-type/filter-type.test.tsx
@@ -2,22 +2,22 @@ import {render, screen} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store, history} from '../mockStore';
-import FilterStringsCount from './filter-strings-count';
+import FilterType from './filter-type';
 
-describe('Component: FilterStringsCount', () => {
+describe('Component: FilterType', () => {
   const fakeOnChange = jest.fn();
   const fakeGuitarTypes = [''];
   const fakeGuitarCounts = [3];
 
-  it('should render FilterStringsCount component', () => {
+  it('should render FilterType component', () => {
     render(
       <Provider store={store}>
         <Router history={history}>
-          <FilterStringsCount onChange={fakeOnChange} newGuitarTypes={fakeGuitarTypes} newGuitarCounts={fakeGuitarCounts}/>
+          <FilterType onChange={fakeOnChange} newGuitarCounts={fakeGuitarCounts} newGuitarTypes={fakeGuitarTypes}/>
         </Router>
       </Provider>,
     );
 
-    expect(screen.getByText(/Количество струн/i)).toBeInTheDocument();
+    expect(screen.getByText(/Тип гитар/i)).toBeInTheDocument();
   });
 } );
