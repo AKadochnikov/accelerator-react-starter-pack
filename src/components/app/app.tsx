d1c43b252735c52c068c8e503bd4032e9ba101c7
Alexander <alex.kadoh@mail.ru>
2022-02-04 14:18:30 +0700

    create product component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from '../main/main';
 import NotFound from '../404-not-found/404';
 import {AppRoute} from '../../const';
+import Product from '../product';
 
 function App(): JSX.Element {
   return (
@@ -12,6 +13,9 @@ function App(): JSX.Element {
         </Route>
         <Route exact path={AppRoute.Catalog}>
           <Main/>
+        </Route>
+        <Route exact path={AppRoute.Guitar}>
+          <Product/>
         </Route>
         <Route>
           <NotFound />
