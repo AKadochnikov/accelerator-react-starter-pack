f98f27a755d424e1fca3f3cdc58725c7470e1fa4
Alexander <alex.kadoh@mail.ru>
2022-02-08 21:29:13 +0700

    create custom Validity addComment Modal form

--- src/components/add-comment-modal/add-comment-modal.tsx
+++ src/components/add-comment-modal/add-comment-modal.tsx
@@ -1,6 +1,7 @@
-import {Dispatch, SetStateAction} from 'react';
+import {Dispatch, SetStateAction, useState} from 'react';
 import {Key} from '../../const';
 import FocusTrap from 'focus-trap-react';
+import {FormEvent, MouseEvent} from 'react';
 
 type AddCommentModalProps = {
   setIsOpenedCommentModal: Dispatch<SetStateAction<boolean>>
@@ -10,6 +11,11 @@ type AddCommentModalProps = {
 
 function AddCommentModal(props: AddCommentModalProps): JSX.Element {
   const {setIsOpenedCommentModal, guitarName} = props;
+  const [isValidRating, setIsValidRating] = useState<boolean>(true);
+  const [isValidInput, setIsValidInput] = useState<boolean>(true);
+  const [isDisabledSubmit, setIsDisabledSubmit] = useState<boolean>(false);
+  const [nameValue, setNameValue] = useState<string>('');
+  const [ratingValue, setRatingValue] = useState<number>(0);
 
   const handleKeyDown = (evt: KeyboardEvent) => {
     if(evt.key === Key.Escape || evt.key === Key.Esc) {
@@ -24,6 +30,43 @@ function AddCommentModal(props: AddCommentModalProps): JSX.Element {
     setIsOpenedCommentModal(false);
     document.body.removeEventListener('keydown', handleKeyDown);
   };
+
+  const handleInput = (evt: FormEvent<HTMLInputElement>) => {
+    const currentValue = evt.currentTarget.value;
+    setNameValue(currentValue);
+    if (currentValue === '') {
+      setIsDisabledSubmit(true);
+      setIsValidInput(false);
+    } else if (ratingValue === 0) {
+      setIsDisabledSubmit(true);
+      setIsValidRating(false);
+    } else {
+      setIsDisabledSubmit(false);
+      setIsValidInput(true);
+    }
+  };
+
+  const handleRating = (evt: MouseEvent<HTMLInputElement>) => {
+    const currentValue = Number(evt.currentTarget.value);
+    setRatingValue(currentValue);
+    if (currentValue === 0) {
+      setIsDisabledSubmit(true);
+      setIsValidRating(false);
+    } else if (nameValue === '') {
+      setIsDisabledSubmit(true);
+      setIsValidInput(false);
+    } else {
+      setIsDisabledSubmit(false);
+      setIsValidRating(true);
+    }
+  };
+
+  const handleSubmit = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log('Hello');
+  };
+
   return (
     <FocusTrap>
       <div className="modal is-active modal--review">
@@ -36,23 +79,23 @@ function AddCommentModal(props: AddCommentModalProps): JSX.Element {
               <div className="form-review__wrapper">
                 <div className="form-review__name-wrapper">
                   <label className="form-review__label form-review__label--required" htmlFor="user-name">Ваше Имя</label>
-                  <input className="form-review__input form-review__input--name" id="user-name" type="text" autoComplete="off"/>
-                  <span className="form-review__warning">Заполните поле</span>
+                  <input onInput={handleInput} className="form-review__input form-review__input--name" id="user-name" type="text" autoComplete="off"/>
+                  <span className="form-review__warning" aria-live='polite'>{isValidInput? '' : 'Заполните поле'}</span>
                 </div>
                 <div><span className="form-review__label form-review__label--required">Ваша Оценка</span>
                   <div className="rate rate--reverse">
-                    <input className="visually-hidden" type="radio" id="star-5" name="rate" value="5"/>
+                    <input onClick={handleRating} className="visually-hidden" type="radio" id="star-5" name="rate" value="5"/>
                     <label className="rate__label" htmlFor="star-5" title="Отлично"/>
-                    <input className="visually-hidden" type="radio" id="star-4" name="rate" value="4"/>
+                    <input onClick={handleRating} className="visually-hidden" type="radio" id="star-4" name="rate" value="4"/>
                     <label className="rate__label" htmlFor="star-4" title="Хорошо"/>
-                    <input className="visually-hidden" type="radio" id="star-3" name="rate" value="3"/>
+                    <input onClick={handleRating} className="visually-hidden" type="radio" id="star-3" name="rate" value="3"/>
                     <label className="rate__label" htmlFor="star-3" title="Нормально"/>
-                    <input className="visually-hidden" type="radio" id="star-2" name="rate" value="2"/>
+                    <input onClick={handleRating} className="visually-hidden" type="radio" id="star-2" name="rate" value="2"/>
                     <label className="rate__label" htmlFor="star-2" title="Плохо"/>
-                    <input className="visually-hidden" type="radio" id="star-1" name="rate" value="1"/>
+                    <input onClick={handleRating} className="visually-hidden" type="radio" id="star-1" name="rate" value="1"/>
                     <label className="rate__label" htmlFor="star-1" title="Ужасно"/>
                     <span className="rate__count"/>
-                    <span className="rate__message">Поставьте оценку</span>
+                    <span className="rate__message" aria-live='polite'>{isValidRating? '' : 'Поставьте оценку'}</span>
                   </div>
                 </div>
               </div>
@@ -62,7 +105,7 @@ function AddCommentModal(props: AddCommentModalProps): JSX.Element {
               <input className="form-review__input" id="user-name" type="text" autoComplete="off"/>
               <label className="form-review__label" htmlFor="user-name">Комментарий</label>
               <textarea className="form-review__input form-review__input--textarea" id="user-name" rows={10} autoComplete="off"/>
-              <button className="button button--medium-20 form-review__button" type="submit">Отправить отзыв</button>
+              <button onClick={handleSubmit} className="button button--medium-20 form-review__button" type="submit" disabled={isDisabledSubmit}>Отправить отзыв</button>
             </form>
             <button onClick={handleCloseClick} className="modal__close-btn button-cross" type="button" aria-label="Закрыть">
               <span className="button-cross__icon"/>
