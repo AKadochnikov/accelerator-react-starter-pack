ba4a86480490a98ffc0b47e8311a16bba7d0c77d
Alexander <alex.kadoh@mail.ru>
2022-02-09 17:22:16 +0700

    add test for useFetchGuitar

---
+++ src/hooks/useFetchGuitar.test.ts
@@ -0,0 +1,25 @@
+import {renderHook} from '@testing-library/react-hooks';
+import {useFetchGuitar} from './useFetchGuitar';
+
+const id = '1';
+const fakeId = '1ol';
+
+describe('Hook: useFetchGuitar', () => {
+  it('should object and loadStatus',  () => {
+    const {result} = renderHook(() => useFetchGuitar(id));
+
+    const {guitar, loadStatus} = result.current;
+
+    expect(typeof guitar).toBe('object');
+    expect(typeof loadStatus).toBe('string');
+  });
+
+  it('should null and loadStatus if take fakeId',  () => {
+    const {result} = renderHook(() => useFetchGuitar(fakeId));
+
+    const {guitar, loadStatus} = result.current;
+
+    expect(guitar).toBe(null);
+    expect(typeof loadStatus).toBe('string');
+  });
+});
