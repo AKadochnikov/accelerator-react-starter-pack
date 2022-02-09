0cd90c62793c9e41479ec4773f9735e648766db9
Alexander <alex.kadoh@mail.ru>
2022-02-09 22:36:31 +0700

    create test for rating component

---
+++ src/components/rating/rating.test.tsx
@@ -0,0 +1,15 @@
+import {render, screen} from '@testing-library/react';
+import Rating from './rating';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {fakeGuitar} from '../../mockGuitars';
+const history = createMemoryHistory();
+
+
+describe('Component: Rating', () => {
+  it('should is rendered component', () => {
+    render(<Router history={history}><Rating rating={fakeGuitar.rating} id={fakeGuitar.id} comments={fakeGuitar.comments}/></Router>);
+
+    expect(screen.getByText(`${fakeGuitar.comments.length}`)).toBeInTheDocument();
+  });
+});
