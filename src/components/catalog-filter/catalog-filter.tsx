0245154be778c3d8e959bc6a6cff1cf36413b653
Alexander <alex.kadoh@mail.ru>
2022-03-09 16:26:31 +0700

    delete key props in FilterType and FilterStringsCount

--- src/components/catalog-filter/catalog-filter.tsx
+++ src/components/catalog-filter/catalog-filter.tsx
@@ -2,7 +2,6 @@ import FilterPrice from '../filter-price/filter-price';
 import FilterType from '../filter-type/filter-type';
 import FilterStringsCount from '../filter-strings-count/filter-strings-count';
 import {useState} from 'react';
-import {Params} from '../../const';
 import {useTypesCountsGuitar} from '../../hooks/use-types-counts-guitar/use-types-counts-guitar';
 
 function CatalogFilter (): JSX.Element {
@@ -14,8 +13,8 @@ function CatalogFilter (): JSX.Element {
     <form className="catalog-filter">
       <h2 className="title title--bigger catalog-filter__title">Фильтр</h2>
       <FilterPrice/>
-      <FilterType key={Params.GuitarType} newGuitarCounts={newGuitarCounts} newGuitarTypes={newGuitarTypes} onChangeTypes={setNewGuitarTypes} onChangeCounts={setNewGuitarCounts}/>
-      <FilterStringsCount key={Params.StringCount} newGuitarCounts={newGuitarCounts} newGuitarTypes={newGuitarTypes} onChangeCounts={setNewGuitarCounts} onChangeTypes={setNewGuitarTypes}/>
+      <FilterType newGuitarCounts={newGuitarCounts} newGuitarTypes={newGuitarTypes} onChangeTypes={setNewGuitarTypes} onChangeCounts={setNewGuitarCounts}/>
+      <FilterStringsCount newGuitarCounts={newGuitarCounts} newGuitarTypes={newGuitarTypes} onChangeCounts={setNewGuitarCounts} onChangeTypes={setNewGuitarTypes}/>
     </form>
   );
 }
